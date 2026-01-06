edf07202e449fe85c833058f00707c218d9fdc7f
THAITECHNEWS <github@thaitechnews.com>
2026-01-06 21:39:36 +0200

    Replace credit-cards menu link with compare-credit-cards page

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
         </div>
         
         <div className="nav-menu">
-          <Link href="/credit-cards" className="nav-link">
+          <Link href="/compare-credit-cards" className="nav-link">
             Credit Cards
           </Link>
           
